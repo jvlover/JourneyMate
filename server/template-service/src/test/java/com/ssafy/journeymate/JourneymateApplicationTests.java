207a40946b3ffccd6a167cdb4d57f5ed6a6793b1
노성호 <shtjdgh00@gmail.com>
2023-10-19 17:57:26 +0900

    Feat: 프로젝트 생성
    프로젝트 구조 생성 및 gradle dependency 추가

---
+++ server/template-service/src/test/java/com/ssafy/journeymate/JourneymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.journeymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
