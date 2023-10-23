23e6e17087057ee5d13f28fd6a4006e789d843fb
김도현 <97046334+fnejd@users.noreply.github.com>
2023-10-23 17:28:01 +0900

    Feat:Initialize mate service

---
+++ mate-service/src/main/java/com/ssafy/journeymate/mateservice/MateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.journeymate.mateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateServiceApplication.class, args);
+	}
+
+}
---
+++ mate-service/src/test/java/com/ssafy/journeymate/mateservice/MateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.journeymate.mateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
