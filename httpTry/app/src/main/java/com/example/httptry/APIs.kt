1ce5a9b2f78d4a2477e356ed3e9f0b7a7549a88d
kcrikana <beom0109@naver.com>
2023-11-06 17:55:16 +0900

    Feat: comment-service추가

--- httpTry/app/src/main/java/com/example/httptry/APIs.kt
+++ httpTry/app/src/main/java/com/example/httptry/APIs.kt
@@ -3,16 +3,27 @@ package com.example.httptry
 import android.text.Editable
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.Date
 
 data class Error(val code: String, val message: String)
 data class UserLoginPostRes(val userId: Long, val loginId: String, val nickname: String)
 // 전달 형식 어떻게?
 data class LoginResponse(val success: Boolean, val data: UserLoginPostRes, val error: Error)
 
+data class CommentLoadGetRes(val commentId: String, val mateId: String, val sender: String, val message: String, val userCount: Long, val timeStampe: Date)
+
+data class LoadCommentResponse(val success: Boolean, val data: CommentLoadGetRes, val error: Error)
+
 interface ApiService {
     @POST("/user/login")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    @GET("/comment-service/{mateId}")
+    fun loadComment(@Path(value = "mateId") mateId : String): Call<List<LoadCommentResponse>>
 }
 
 data class LoginRequest(val loginId: String, val loginPassword: String)
+
--- httpTry/app/src/main/java/com/example/httptry/MainActivity.kt
+++ httpTry/app/src/main/java/com/example/httptry/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://xx.xx.xxx.xx:8080")
+            .baseUrl("http://70.12.246.220:8080")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
@@ -31,9 +31,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.button1.setOnClickListener {
             val loginRequest = LoginRequest(binding.idInput.text.toString(), binding.pwInput.text.toString())
-            val call: Call<LoginResponse> = apiService.login(loginRequest)
+            val callLogin: Call<LoginResponse> = apiService.login(loginRequest)
 
-            call.enqueue(object: Callback<LoginResponse> {
+            callLogin.enqueue(object: Callback<LoginResponse> {
                 override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                     val result = response.body() as LoginResponse?
                     if( result == null ) {
@@ -50,5 +50,32 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }
+
+        binding.button2.setOnClickListener {
+            val mateId = binding.mateIdInput.text.toString()
+            val callLoadComment: Call<List<LoadCommentResponse>> = apiService.loadComment(mateId)
+            callLoadComment.enqueue(object: Callback<List<LoadCommentResponse>> {
+                override fun onResponse(
+                    call: Call<List<LoadCommentResponse>>,
+                    response: Response<List<LoadCommentResponse>>
+                ) {
+                    val result = response.body()
+                    if(result.isNullOrEmpty()) binding.textView2.text = "잘안됨"
+                    else {
+                        val commentsText = result.joinToString("\n") { comment ->
+                            "보낸이: ${comment.data.sender}, 메시지: ${comment.data.message}, 시간: ${comment.data.timeStampe}"
+                        }
+                        binding.textView2.text = commentsText
+                    }
+                }
+
+                override fun onFailure(call: Call<List<LoadCommentResponse>>, t: Throwable) {
+                    binding.textView2.text = "에러"
+                    Log.e("메인 액티비티 에러", "onFailure: ", t)
+                }
+            })
+        }
+
+
     }
 }
