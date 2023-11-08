e7236f20288744832cadde6c85d57b831cc83ba4
김민범 <beom0109@naver.com>
2023-11-08 17:40:35 +0900

    Feat: 메인화면 추가

--- journeymate/app/src/main/java/com/ssafy/journeymate/MainActivity.kt
+++ journeymate/app/src/main/java/com/ssafy/journeymate/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ssafy.journeymate
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.ssafy.journeymate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -29,12 +32,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-        val intent = Intent(this,LoadScreanActivity::class.java)
+        val intent = Intent(this,LoadScreenActivity::class.java)
         startActivity(intent)
     }
 
