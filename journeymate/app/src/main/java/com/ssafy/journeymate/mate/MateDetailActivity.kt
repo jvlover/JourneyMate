f2b3924a705d079bf515fdb02f5b9550222e3f24
김도현 <97046334+fnejd@users.noreply.github.com>
2023-11-13 02:35:57 +0900

    Feat: mate-list retrofit 추가

--- journeymate/app/src/main/java/com/ssafy/journeymate/mate/MateDetailActivity.kt
+++ journeymate/app/src/main/java/com/ssafy/journeymate/mate/MateDetailActivity.kt
@@ -2,11 +2,62 @@ package com.ssafy.journeymate.mate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import com.ssafy.journeymate.R
+import com.ssafy.journeymate.api.LoadMateInfoResponse
+import com.ssafy.journeymate.api.MateApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MateDetailActivity : AppCompatActivity() {
+    private lateinit var retrofit: Retrofit
+    private lateinit var mateApi: MateApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mate_detail)
+
+        val mateId = 2L
+
+        // Retrofit instance 생성
+        retrofit = Retrofit.Builder()
+            .baseUrl("http://k9a204.p.ssafy.io:8000//mate-service/{$mateId}") // 여기에 실제 API 서버 주소를 넣어야 합니다.
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        mateApi = retrofit.create(MateApi::class.java)
+
+        // API 호출
+        val call = mateApi.loadMateInfo(mateId) // 여기에 실제 id 값을 넣어야 합니다.
+        call.enqueue(object : Callback<LoadMateInfoResponse> {
+            override fun onResponse(call: Call<LoadMateInfoResponse>, response: Response<LoadMateInfoResponse>) {
+                if (response.isSuccessful) {
+                    val responseData = response.body()?.data
+
+                    // mate 이름
+                    val mateNameTextView = findViewById<TextView>(R.id.detail_mate_name)
+                    mateNameTextView.text = responseData?.name
+
+                    // userProfile의 nickname 수만큼 프로필 필요
+                    val mateDetailActivity = findViewById<ConstraintLayout>(R.id.mate_detail_layout)
+                    val inflater = LayoutInflater.from(this@MateDetailActivity)
+
+
+                }
+            }
+
+            override fun onFailure(call: Call<LoadMateInfoResponse>, t: Throwable) {
+                Log.e("error log", "실패했습니다. ")
+            }
+        })
     }
 }
--- journeymate/app/src/main/java/com/ssafy/journeymate/mate/MateListActivity.kt
+++ journeymate/app/src/main/java/com/ssafy/journeymate/mate/MateListActivity.kt
@@ -1,12 +1,67 @@
 package com.ssafy.journeymate.mate
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.ssafy.journeymate.R
+import com.ssafy.journeymate.api.FindDocsListResponse
+import com.ssafy.journeymate.api.UserApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MateListActivity : AppCompatActivity() {
+
+    private lateinit var userApi: UserApi
+    private lateinit var retrofit: Retrofit
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mate_list)
+
+
+        val userId = "11ee7c7b3842d729b20e1d0553c96277"
+
+        retrofit = Retrofit.Builder()
+            .baseUrl("http://k9a204.p.ssafy.io:8000/user-service/mate/{$userId}/") // 여기에 실제 API 서버 주소를 넣어야 합니다.
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+        userApi = retrofit.create(UserApi::class.java)
+
+        val mateListLayout = findViewById<LinearLayout>(R.id.mate_list_layout)
+        val inflater = LayoutInflater.from(this)
+
+        val call = userApi.findDocsById(userId) // 여기에 실제 id 값을 넣어야 합니다.
+        call.enqueue(object : Callback<FindDocsListResponse> {
+            override fun onResponse(call: Call<FindDocsListResponse>, response: Response<FindDocsListResponse>) {
+                if (response.isSuccessful) {
+                    val responseData = response.body()?.data?.docsInfoList
+                    responseData?.forEach { data ->
+
+                        val view = inflater.inflate(R.layout.layout_mate_list_data, mateListLayout, false)
+
+                        val textView = view.findViewById<TextView>(R.id.mate_name)
+                        textView.text = data.title // 제목 설정
+
+
+                        mateListLayout.addView(view)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<FindDocsListResponse>, t: Throwable) {
+                Log.e("error log", "실패했습니다. ")
+            }
+        })
+
     }
+
 }
