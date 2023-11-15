f8f05e4bf0f45328e7aa0769a9b86d1efc97a1e9
김도현 <97046334+fnejd@users.noreply.github.com>
2023-11-15 23:42:32 +0900

    Feat: MateDetailRes json format 추가

--- mate-service/src/main/java/com/ssafy/journeymate/mateservice/dto/response/mate/MateDetailRes.java
+++ mate-service/src/main/java/com/ssafy/journeymate/mateservice/dto/response/mate/MateDetailRes.java
@@ -1,5 +1,10 @@
 package com.ssafy.journeymate.mateservice.dto.response.mate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Builder;
@@ -12,7 +17,15 @@ public class MateDetailRes {
 
     private Long mateId;
     private String name;
+
+    @JsonSerialize(using =  LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
+
+    @JsonSerialize(using =  LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endDate;
     private List<String> users;
     private String creator;
