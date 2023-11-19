a4c84b8bbe62de0fad13ecd8850eee9e945e4ef3
Juseok <as2835510@hanyang.ac.kr>
2023-11-19 16:22:00 +0900

    Feat: docs write api 연결 완료

--- journeymate/app/src/main/java/com/ssafy/journeymate/mate/DocsWriteActivity.kt
+++ journeymate/app/src/main/java/com/ssafy/journeymate/mate/DocsWriteActivity.kt
@@ -2,16 +2,42 @@ package com.ssafy.journeymate.mate
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.journeymate.R
+import com.ssafy.journeymate.api.FindMateData
+import com.ssafy.journeymate.api.MateApi
+import com.ssafy.journeymate.api.RegistDocsResponse
+import com.ssafy.journeymate.global.App
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.FileOutputStream
 
 class DocsWriteActivity : AppCompatActivity() {
+
+    private lateinit var retrofit: Retrofit
+    private lateinit var mateApi: MateApi
+
+    var mateData: FindMateData? = null
+    private var selectedImageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_docs_write)
@@ -21,6 +47,16 @@ class DocsWriteActivity : AppCompatActivity() {
         val toolbarTitleTextView = toolbarInclude.findViewById<TextView>(R.id.toolbarTitle)
 
         toolbarTitleTextView.text = "여행 문서 작성"
+
+        val backButton: ImageButton = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            // 뒤로 가기 버튼 클릭
+            onBackPressed()
+        }
+
+        // mateId 전역변수 꺼내오기
+        mateData = intent.getSerializableExtra("mateData") as FindMateData
+
         val AddBtn = findViewById<Button>(R.id.AddBtn)
         val docsBtn = findViewById<Button>(R.id.docs_regist_btn)
 
@@ -30,7 +66,62 @@ class DocsWriteActivity : AppCompatActivity() {
             startActivityForResult(intent, 100)
         }
 
+        retrofit = Retrofit.Builder()
+            .baseUrl("http://k9a204.p.ssafy.io:8000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        mateApi = retrofit.create(MateApi::class.java)
+
         docsBtn.setOnClickListener {
+
+            val titleEditText: EditText = findViewById(R.id.mate_docs_title_input)
+            val contentEditText: EditText = findViewById(R.id.mate_docs_content_input)
+
+            val title = titleEditText.text.toString()
+            val content = contentEditText.text.toString()
+
+            val mateId = mateData!!.mateId
+            val userId = App.INSTANCE.id
+
+            val requestMap = HashMap<String, RequestBody>().apply {
+                put("title", RequestBody.create("text/plain".toMediaTypeOrNull(), title))
+                put("content", RequestBody.create("text/plain".toMediaTypeOrNull(), content))
+                put("userId", RequestBody.create("text/plain".toMediaTypeOrNull(), userId))
+                put("mateId", RequestBody.create("text/plain".toMediaTypeOrNull(), mateId.toString()))
+            }
+
+            val imagePart = selectedImageUri?.let { uri ->
+                // 여기서 이미지 파일을 MultipartBody.Part로 변환
+                val inputStream = contentResolver.openInputStream(uri)
+                val file = File(cacheDir, "tempImage")
+                val outputStream = FileOutputStream(file)
+                inputStream?.copyTo(outputStream)
+
+                val requestFile = RequestBody.create("image/jpeg".toMediaTypeOrNull(), file)
+                MultipartBody.Part.createFormData("imgFile", file.name, requestFile)
+            }
+
+            val call = mateApi.registDocs(requestMap, listOfNotNull(imagePart))
+            call.enqueue(object : Callback<RegistDocsResponse> {
+                override fun onResponse(call: Call<RegistDocsResponse>, response: Response<RegistDocsResponse>) {
+                    if (response.isSuccessful) {
+                        Log.d("DOCS_WRITE", "문서 작성 완료")
+                        val intent = Intent(this@DocsWriteActivity, DocsListActivity::class.java)
+                        // 전역 변수에 mateId추가
+                        intent.putExtra("mateData", mateData)
+                        startActivity(intent)
+                    } else {
+                        Log.d("DOCS_WRITE", "문서 작성 실패!!")
+                    }
+                }
+
+                override fun onFailure(call: Call<RegistDocsResponse>, t: Throwable) {
+                    Log.d("DOCS_WRITE", "문서 작성 요청 실패: ${t.message}")
+                }
+            })
+
+
 
         }
 
@@ -40,7 +131,7 @@ class DocsWriteActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
-            val selectedImageUri = data?.data // 선택된 이미지의 URI
+            selectedImageUri = data?.data // 선택된 이미지의 URI
 
             // ImageView에 이미지 표시
             val imageView: ImageView = findViewById(R.id.selectedImageView)
