22111d69e80d9586c6d0eede110f1a34a010b144
Juseok <as2835510@hanyang.ac.kr>
2023-11-19 21:35:07 +0900

    Feat: chat activity chat title, mateId, userCount 변경

--- journeymate/app/src/main/java/com/ssafy/journeymate/api/ChatApi.kt
+++ journeymate/app/src/main/java/com/ssafy/journeymate/api/ChatApi.kt
@@ -3,21 +3,10 @@ package com.ssafy.journeymate.api
 
 import com.gmail.bishoybasily.stomp.lib.constants.*
 import com.gmail.bishoybasily.stomp.lib.*
-import okhttp3.HttpUrl
-import okhttp3.MultipartBody
-import okhttp3.OkHttpClient
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Part
 import retrofit2.http.Path
 import java.io.Serializable
-import java.time.LocalDateTime
-import java.util.Objects
 
 
 data class ChatResponse(val message: String, val data: ChatData) : Serializable
@@ -48,7 +37,7 @@ data class SendCommentRequest(val chat: ChatData)
 interface ChatApi {
 
     @GET("/chat-service/{mateId}")
-    fun loadComment(@Path("mateId") mateId: Long): Call<ResponseDto>
+    fun loadComment(@Path("mateId") mateId: String): Call<ResponseDto>
 
 
 }
